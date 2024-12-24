cf9a13560a456971ee651cd12609c112459ee587
xuebusi <xuebusi@qq.com>
2024-12-24 13:42:14 +0800

    头部布局

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
-<template>
 
+<script setup>
+import MainTopToolbar from './components/toolbar/MainTopToolbar.vue';
+</script>
+
+<template>
   <n-layout position="absolute">
     <n-layout-header bordered style="height: 64px;padding: 0 20px">
-      颐和园路
+      <main-top-toolbar />
     </n-layout-header>
     <n-layout position="absolute" style="top: 64px;" has-sider>
       <n-layout-sider bordered width="64px" content-style="padding: 24px 0; text-align: center">
@@ -13,8 +17,4 @@
       </n-layout-content>
     </n-layout>
   </n-layout>
-
 </template>
-
-<script setup>
-</script>
---
+++ src/components/toolbar/MainTopToolbar.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { DarkModeRound, NotificationsNoneOutlined } from '@vicons/material'
+</script>
+
+<template>
+    <n-space align="center" justify="space-between" style="height: 100%;">
+        <n-text>知科笔记</n-text>
+        <n-space>
+            <!-- 头像 -->
+            <n-avatar
+            round
+            size="small"
+            src="https://07akioni.oss-cn-beijing.aliyuncs.com/07akioni.jpeg"
+            style="position: relative; top: 5px;"
+            />
+
+            <!-- 分割线 -->
+            <n-divider vertical style="position: relative; top: 5px;" />
+
+            <!-- 消息按钮 -->
+            <n-badge dot type="success" processing :offset="[-8, 4]">
+                <n-button circle tertiary>
+                    <n-icon size="18" :component="NotificationsNoneOutlined" />
+                </n-button>
+            </n-badge>
+
+            <!-- 主题按钮 -->
+            <n-button tertiary circle>
+                <n-icon size="18" :component="DarkModeRound" />
+            </n-button>
+
+            <!-- 登录按钮 -->
+            <n-button tertiary type="primary">登录</n-button>
+        </n-space>
+    </n-space>
+</template>
